c129eaebe9eb8decc9bb1ae4574bc71269037f99
MEH ENGELBERT KUM <menbongtah@gmail.com>
2026-01-19 20:18:12 +0100

    Fix controllers and config + pipeline update

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,7 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HomeController {
+
+    @GetMapping("/")
+    public String root() {
+        return "OK";
+    }
 
     @GetMapping("/health")
     public String health() {
